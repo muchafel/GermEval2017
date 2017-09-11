b9d7ca968583d0e6d0f4e4cfefb8486c8d21ea54
Michael Wojatzki <michael.wojatzki@googlemail.com>
2017-09-11 08:43:53 -0400

    updating jar

--- src/main/java/de/germeval2017/Evaluation/main/GermevalEvaluator.java
+++ src/main/java/de/germeval2017/Evaluation/main/GermevalEvaluator.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import de.germeval2017.Evaluation.EvaluationUtil.ConfusionMatrix;
 import de.germeval2017.Evaluation.EvaluationUtil.EvaluationData;
 import de.germeval2017.Evaluation.EvaluationUtil.Fscore;
 import de.germeval2017.Evaluation.EvaluationUtil.OTEMatcher;
